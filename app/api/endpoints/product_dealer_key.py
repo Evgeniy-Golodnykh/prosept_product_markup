9bc6e413e717cbe08a000880040f41c24b0e666b
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-11-29 01:26:37 +0300

    add validation

--- app/api/endpoints/product_dealer_key.py
+++ app/api/endpoints/product_dealer_key.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.validators import check_dealer_price_exists, check_product_exists
 from app.core.db import get_async_session
 from app.crud import product_dealer_key_crud
 from app.schemas.product_dealer_key import (
@@ -14,7 +15,7 @@ router = APIRouter()
     '/',
     response_model=list[ProducDealerKeyDB],
 )
-async def get_all_keys(
+async def get_all_markups(
         session: AsyncSession = Depends(get_async_session),
 ):
     return await product_dealer_key_crud.get_multi(session)
@@ -24,8 +25,10 @@ async def get_all_keys(
     '/',
     response_model=ProducDealerKeyDB,
 )
-async def create_key(
-        key: ProductDealerKeyCreate,
+async def create_markup(
+        markup: ProductDealerKeyCreate,
         session: AsyncSession = Depends(get_async_session),
 ):
-    return await product_dealer_key_crud.create(key, session)
+    await check_product_exists(markup.product_id, session)
+    await check_dealer_price_exists(markup.key, session)
+    return await product_dealer_key_crud.create(markup, session)
--- app/api/validators.py
+++ app/api/validators.py
@@ -1,11 +1,11 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud import dealer_crud, dealer_price_crud
-from app.models import DealerPrice
+from app.crud import dealer_crud, dealer_price_crud, product_crud
 
 DEALER_EXISTS_ERROR = 'Дилер с таким именем уже существует!'
-DEALER_PRICE_NOT_EXISTS_ERROR = 'Товар с id "{dealer_price_id}" не найден!'
+DEALER_PRICE_NOT_EXISTS_ERROR = 'Товар маркетплейса с ключом "{key}" не найден'
+PRODUCT_NOT_EXISTS_ERROR = 'Товар заказчика с id "{id}" не найден'
 
 
 async def check_dealer_name_duplicate(
@@ -21,17 +21,26 @@ async def check_dealer_name_duplicate(
 
 
 async def check_dealer_price_exists(
-        dealer_price_id: int,
+        dealer_price_key: int,
         session: AsyncSession,
-) -> DealerPrice:
-    dealer_price = await dealer_price_crud.get(
-        dealer_price_id, session
+) -> None:
+    dealer_price_id = await dealer_price_crud.get_dealer_price_id_by_key(
+        dealer_price_key, session
     )
-    if dealer_price is None:
+    if dealer_price_id is None:
         raise HTTPException(
             status_code=404,
-            detail=DEALER_PRICE_NOT_EXISTS_ERROR.format(
-                dealer_price_id=dealer_price_id
-            )
+            detail=DEALER_PRICE_NOT_EXISTS_ERROR.format(key=dealer_price_id)
         )
-    return dealer_price
+
+
+async def check_product_exists(
+        product_id: int,
+        session: AsyncSession,
+) -> None:
+    product = await product_crud.get(product_id, session)
+    if product is None:
+        raise HTTPException(
+            status_code=404,
+            detail=PRODUCT_NOT_EXISTS_ERROR.format(id=product_id)
+        )
