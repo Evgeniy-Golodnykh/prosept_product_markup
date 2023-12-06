f721f000c24c11a8de4f7af1b71c066f934d2d51
Evgeniy Golodnykh <113941793+Evgeniy-Golodnykh@users.noreply.github.com>
2023-12-06 11:47:31 +0300

    Update recommendation.py

--- app/schemas/recommendation.py
+++ app/schemas/recommendation.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel
 
 class RecommendationCreate(BaseModel):
     product_key: str
-    product_id: list[int]
-    pred_sim: list[float]
+    product_id: list[int] = []
+    pred_sim: list[float] = []
