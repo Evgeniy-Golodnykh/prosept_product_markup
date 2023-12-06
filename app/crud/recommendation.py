d0d7296edb02465477bf859d64ca4816ae989355
Evgeniy Golodnykh <113941793+Evgeniy-Golodnykh@users.noreply.github.com>
2023-12-06 17:15:20 +0300

    Update recommendation.py

--- app/crud/recommendation.py
+++ app/crud/recommendation.py
@@ -1,9 +1,25 @@
+from typing import Optional
+
+from sqlalchemy import desc, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.base import CRUDBase
 from app.models import Recommendation
 
 
 class CRUDRecommendation(CRUDBase):
-    pass
+
+    async def get_recommendation_by_key(
+            self,
+            key: str,
+            session: AsyncSession,
+    ) -> Optional[Recommendation]:
+        db_recommendation = await session.execute(
+            select(Recommendation).where(
+                Recommendation.product_key == key
+            ).order_by(desk(Recommendation.id))
+        )
+        return db_recommendation.scalars().first()
 
 
 recommendation_crud = CRUDRecommendation(Recommendation)
