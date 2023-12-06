5763c90b7f92ca9df9c0ed2802b4ed25e22dd48f
Evgeniy Golodnykh <evgenferrum@gmail.com>
2023-12-07 00:45:25 +0300

    add DS

--- ds/main.py
+++ ds/main.py
@@ -9,10 +9,10 @@ def main():
 
     prosept = Prosept_func()
 
-    url_dealerprice = 'http://0.0.0.0:8000/api/dealerprice/'
+    url_dealerprice = 'http://127.0.0.1:8000/api/dealerprice/'
     df_dealerprice = prosept.preprocess_dealerprice(url_dealerprice)
 
-    url_product = 'http://0.0.0.0:8000/api/product/'
+    url_product = 'http://127.0.0.1:8000/api/product/'
     df_product = prosept.preprocess_product(url_product)
 
     path_to_dir = 'data/'
@@ -31,7 +31,7 @@ def main():
 
     df_result, json_result = prosept.result_to_df_json(pred_id_key, pred_sim, df_dealerprice)
 
-    prosept.save_json('http://0.0.0.0:8000/api/recommendation/', json_result)
+    prosept.save_json('http://127.0.0.1:8000/api/recommendation/', json_result)
 
     df_metric = df_result.merge(df_productdealerkey[['key', 'product_id']], left_on='product_key', right_on='key')
     print(prosept.metric(df_metric.product_id_y, df_metric.product_id_x, 5))
