4c95b34b0d91101b276f409fe05a899d768a43e3
Evgeniy Golodnykh <113941793+Evgeniy-Golodnykh@users.noreply.github.com>
2023-12-06 12:50:19 +0300

    Update recommendation.py

--- app/schemas/recommendation.py
+++ app/schemas/recommendation.py
@@ -7,3 +7,9 @@ class RecommendationCreate(BaseModel):
     product_key: str
     product_id: List[int]
     pred_sim: List[float]
+
+
+class RecommendationDB(RecommendationCreate):
+
+    class Config:
+        orm_mode = True
